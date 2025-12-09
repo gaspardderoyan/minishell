d609baf7a8bad1fb13afae41b1421fa29c902f02
Gaspard Deroyan <gderoyan@f4r13s8.clusters.42paris.fr>
2025-12-09 17:17:53 +0100

    feat: added header file with the reuqired structs for the lexer

---
+++ includes/lexer.h
@@ -0,0 +1,16 @@
+typedef enum	e_token_type
+{
+	TOKEN_WORD,
+	TOKEN_PIPE,
+	TOKEN_INPUT,
+	TOKEN_OUTPUT,
+	TOKEN_HEREDOC,
+	TOKEN_APPEND,
+} t_token_type ;
+
+typedef struct s_token
+{
+	char			*value;
+	t_token_type	type;
+	struct s_token	*next;
+} t_token ;
