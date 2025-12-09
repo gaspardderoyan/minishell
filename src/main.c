7ee1ae9bc666f29dfaba3833c34fbd52b6ef023b
Gaspard Deroyan <gderoyan@f4r13s8.clusters.42paris.fr>
2025-12-09 15:02:53 +0100

    chore: added basic header, main and Makefile

--- src/main.c
+++ src/main.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 #include <readline/readline.h>
+#include <readline/history.h>
 
 int	main(int ac, char **av, char **envp)
 {
 	char *line; 
-	while (*envp)
-	{
-		printf("envp: %s\n", *envp);
-		envp++;
-	}
 	while (1)
 	{
 		line = readline("minishell> ");
 		if (line)
+		{
 			printf("line: %s\n", line);
+			add_history(line);
+		}
 	}
 }
