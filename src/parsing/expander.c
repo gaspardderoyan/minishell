04b9e689284c95a73e0ff793e5981d59a55a1967
Gaspard Deroyan <gderoyan@f4r1s4.clusters.42paris.fr>
2025-12-10 18:19:59 +0100

    feat: started working on expander

--- src/parsing/expander.c
+++ src/parsing/expander.c
@@ -1,5 +1,6 @@
 #include "libft.h"
 #include "minishell.h"
+#include "lexer.h"
 
 char *ft_str_replace(char *orig, char *rep, char *start, char *end)
 {
@@ -18,4 +19,27 @@ char *ft_str_replace(char *orig, char *rep, char *start, char *end)
 	ft_memcpy(res + offset, end, ft_strlen_safe(end));
 	res[len] = 0;
 	return (res);
+}
+
+// TODO: function to replace 1st $var with it's content
+void	expand_token(char *token, char **env)
+{
+	// TODO: find first $var name in token (it's 'start' and 'end' pointers)
+	
+	// TODO: find value of $var in **env
+	
+	// TODO: use ft_str_replace to replace the $var w/ it's value
+}
+
+void	expander(t_token *tokens, char **env)
+{
+	while (tokens)
+	{
+		if (tokens->type != TOKEN_WORD || tokens->value[0] != '"' ||
+			tokens->value[ft_strlen_safe(tokens->value)- 1] != '"' ||
+			ft_strchr_safe(tokens->value, '$') == NULL)
+			tokens = tokens->next;
+		else
+			expand_token(tokens->value, env);
+	}
 }
