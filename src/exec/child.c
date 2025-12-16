4db0579808a9350874e21ad7127aad176fea1aff
Marine Gregoire <swaynmg@gmail.com>
2025-12-16 21:13:13 +0100

    correction child.c do_execve

--- src/exec/child.c
+++ src/exec/child.c
@@ -14,19 +14,21 @@ static void	error_exit(t_cmd *cmd, t_data *data, char *msg, int err_code)
 
 static void	do_execve(t_cmd *cmd, t_data *data)
 {
+	char    **env_array;
+
 	if (!cmd->args || !cmd->args[0])
 	{
-		ft_free_array(data->env);
+		//ft_free_array(data->env);
 		ft_lstclear(&data->env_list, free);
 		exit(0);
 	}
 	init_cmd_path(cmd, data);
 	if (!cmd->cmd_path)
 		error_exit(cmd, data, ": command not found\n", 127);
-	if (execve(cmd->cmd_path, cmd->args, data->env) == -1)
+	if (execve(cmd->cmd_path, cmd->args, env_array) == -1)
 	{
 		perror("minishell");
-		ft_free_array(data->env);
+		ft_free_array(env_array);
 		ft_lstclear(&data->env_list, free);
 		exit(126);
 	}
