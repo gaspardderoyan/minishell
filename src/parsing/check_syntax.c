499259634e3ef5f4014ec07f64719ecf970652af
Gaspard Deroyan <gderoyan@f4r14s7.clusters.42paris.fr>
2025-12-17 18:54:29 +0100

    feat: added more syntax errors handling

--- src/parsing/check_syntax.c
+++ src/parsing/check_syntax.c
@@ -6,11 +6,19 @@
 /*   By: gderoyan <gderoyan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/17 17:52:39 by gderoyan          #+#    #+#             */
-/*   Updated: 2025/12/17 18:34:28 by gderoyan         ###   ########.fr       */
+/*   Updated: 2025/12/17 18:54:12 by gderoyan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+static bool	is_redir(t_token_type type)
+{
+	if (type == TOKEN_INPUT || type == TOKEN_OUTPUT
+		|| type == TOKEN_APPEND || type == TOKEN_HEREDOC)
+		return (true);
+	return (false);
+}
 
 int	check_syntax(t_token *tokens)
 {
@@ -21,6 +29,11 @@ int	check_syntax(t_token *tokens)
 		if (tokens->type == TOKEN_PIPE && tokens->next
 			&& tokens->next->type == TOKEN_PIPE)
 			return (ms_error("double pipe", NULL), FAIL);
+		else if (is_redir(tokens->type) && !(tokens->next))
+			return (ms_error("invalid redirection", NULL), FAIL);
+		else if (is_redir(tokens->type) && tokens->next
+			&& tokens->next != TOKEN_WORD)
+			return (ms_error("invalid redirection", NULL), FAIL);
 		tokens = tokens->next;
 	}
 	return (SUCCESS);
