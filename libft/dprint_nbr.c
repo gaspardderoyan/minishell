55ce8632eec97f0e4687508fb39ddb8640e01cf3
Gaspard Deroyan <gderoyan@f6r7s12.clusters.42paris.fr>
2025-12-20 16:52:49 +0100

    feat: added dprintf

---
+++ libft/dprint_nbr.c
@@ -0,0 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   dprint_nbr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgregoir <mgregoir@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/20 12:00:00 by opencode          #+#    #+#             */
+/*   Updated: 2025/12/20 12:00:00 by opencode         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/**
+ * @brief      Prints a decimal integer to a file descriptor.
+ * @param nb   The integer to print.
+ * @param fd   The file descriptor.
+ * @return     The number of characters printed.
+ */
+int	dprint_decimal(int nb, int fd)
+{
+	char	*str;
+	int		len;
+
+	str = ft_itoa(nb);
+	if (!str)
+		return (0);
+	len = dprint_str(str, fd);
+	free(str);
+	return (len);
+}
+
+/**
+ * @brief      Prints an unsigned integer to a file descriptor.
+ * @param nb   The unsigned integer to print.
+ * @param fd   The file descriptor.
+ * @return     The number of characters printed.
+ */
+int	dprint_uint(unsigned int nb, int fd)
+{
+	char	*numbers;
+	int		count;
+
+	numbers = "0123456789";
+	count = 0;
+	if (nb >= 10)
+		count += dprint_uint(nb / 10, fd);
+	count += write(fd, &numbers[nb % 10], 1);
+	return (count);
+}
+
+/**
+ * @brief      Prints an unsigned integer in hexadecimal format to 
+ * 			   a file descriptor.
+ * @param nb   The unsigned integer to print.
+ * @param uppercase If non-zero, uses uppercase letters (A-F), otherwise
+ * 			   lowercase (a-f).
+ * @param fd   The file descriptor.
+ * @return     The number of characters printed.
+ */
+int	dprint_hexa(unsigned int nb, int uppercase, int fd)
+{
+	char	*hexa;
+	int		count;
+
+	count = 0;
+	if (uppercase)
+		hexa = "0123456789ABCDEF";
+	else
+		hexa = "0123456789abcdef";
+	if (nb >= 16)
+		count += dprint_hexa(nb / 16, uppercase, fd);
+	count += write(fd, &hexa[nb % 16], 1);
+	return (count);
+}
---
+++ libft/ft_dprintf.c
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_dprintf.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgregoir <mgregoir@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/20 12:00:00 by opencode          #+#    #+#             */
+/*   Updated: 2025/12/20 12:00:00 by opencode         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/**
+ * @brief      Dispatches the appropriate print function based on the format 
+ *             specifier.
+ * @param ap   The variable argument list.
+ * @param specifier The format specifier character.
+ * @param fd   The file descriptor.
+ * @return     The number of characters printed.
+ */
+static int	ddispatch_format(va_list ap, char specifier, int fd)
+{
+	int	count;
+
+	count = 0;
+	if (specifier == 'c')
+		count = dprint_char((char)va_arg(ap, int), fd);
+	else if (specifier == 's')
+		count = dprint_str(va_arg(ap, char *), fd);
+	else if (specifier == 'p')
+		count = dprint_ptr(va_arg(ap, void *), fd);
+	else if (specifier == 'd' || specifier == 'i')
+		count = dprint_decimal(va_arg(ap, int), fd);
+	else if (specifier == 'u')
+		count = dprint_uint(va_arg(ap, unsigned int), fd);
+	else if (specifier == 'x')
+		count = dprint_hexa(va_arg(ap, unsigned int), 0, fd);
+	else if (specifier == 'X')
+		count = dprint_hexa(va_arg(ap, unsigned int), 1, fd);
+	else if (specifier == '%')
+		count = dprint_char(specifier, fd);
+	return (count);
+}
+
+/**
+ * @brief      A simplified implementation of dprintf.
+ * @param fd   The file descriptor to print to.
+ * @param format The format string containing text and format specifiers.
+ * @param ...  Variable arguments to be formatted and printed.
+ * @return     The total number of characters printed, or -1 on error.
+ */
+int	ft_dprintf(int fd, const char *format, ...)
+{
+	va_list	ap;
+	int		i;
+	int		count;
+
+	if (!format)
+		return (-1);
+	va_start(ap, format);
+	i = 0;
+	count = 0;
+	while (format[i])
+	{
+		if (format[i] == '%' && format[i + 1])
+		{
+			count += ddispatch_format(ap, format[i + 1], fd);
+			i++;
+		}
+		else
+			count += dprint_char(format[i], fd);
+		i++;
+	}
+	va_end(ap);
+	return (count);
+}
