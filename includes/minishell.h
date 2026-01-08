ae55c3ca4d7ba74abbd5e22bcfa8ffeaa3d4ccb8
Gaspard Deroyan <gderoyan@f4r2s5.clusters.42paris.fr>
2026-01-08 11:32:59 +0100

    fix: added the proper declarations in minishell.h

--- includes/minishell.h
+++ includes/minishell.h
@@ -28,6 +28,8 @@
 # include <sys/stat.h>
 # include <errno.h>
 
+extern volatile sig_atomic_t	g_status;
+
 # ifndef PATH_MAX
 #  define PATH_MAX 4096
 # endif
@@ -113,6 +115,8 @@ typedef struct s_data
 	char	*line;
 	int		stdin_backup;
 	int		stdout_backup;
+	int		line_count;
+	int		heredoc_line;
 }	t_data;
 
 /******  BUILTINS - builtin_cd.c  ******/
@@ -217,6 +221,7 @@ void		set_signal_action(void);
 void		reset_signals_default(void);
 
 /******  SIGNALS - signals_heredoc.c  ******/
+void		set_signal_heredoc(void);
 
 /******  UTILS - cleanup.c  ******/
 void		close_all_pipes(t_cmd *cmd_list);
@@ -233,6 +238,11 @@ void		remove_env_node(t_list **env, char *key);
 /******  UTILS - errors.c  ******/
 void		print_error(char *cmd, char *arg, char *msg);
 void		print_error_var(char *cmd, char *arg, char *msg);
+int			print_eof_warning(char *delim, int line_count);
+
+/******  UTILS - heredoc_utils.c  ******/
+char		*generate_heredoc_name(int i);
+int			handle_heredoc_interrupt(t_data *data, int stdin_backup);
 
 /******  UTILS - path.c  ******/
 char		*get_full_path(char *cmd, t_list *env_list);
@@ -242,5 +252,10 @@ void		init_cmd_path(t_cmd *cmd, t_data *data);
 int			set_pipe(t_cmd *cmd);
 void		handle_redir_fds(t_cmd *cmd);
 void		connect_pipes(t_cmd *cmd, int prev_pipe_read);
+
+/******  UTILS - memory.c  ******/
+void		init_data(t_data *data, char **env, int *ac, char ***av);
+void		free_cycle(t_data *data);
+void		free_data(t_data *data);
 
 #endif
